c601beb86f86832f8afba2ecb46c7bbbebe48864
somin <yygs321@naver.com>
2023-05-17 16:50:21 +0900

    [fix] Fix App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,8 @@
     </div>
     </div>
 
-  <div> 내용  </div>
-  <router-view/>
+
+
   
   <!-- footer -->
 <div class="container">
@@ -74,23 +74,6 @@
 
 
 
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a href="#" class="nav-link mx-1" aria-current="page">로그인</a></li>
-          <li class="nav-item"><a href="#" class="nav-link mx-1">회원가입</a></li>
-        </ul>
-      </header>
-        <div class="nav-scroller py-1 mb-2">
-      <nav class="nav d-flex justify-content-between">
-        <button type="button" class="btn btn-outline-dark d-flex justify-content-between"><span class="me-2">관광지</span><i class="fa-solid fa-map-location-dot"></i></button>
-        <button type="button" class="btn btn-outline-dark d-flex justify-content-between"><span class="me-2">숙박</span><i class="fa-solid fa-hotel"></i></button>
-        <button type="button" class="btn btn-outline-dark d-flex justify-content-between"><span class="me-2">쇼핑</span><i class="fa-solid fa-cart-shopping"></i></button>
-        <button type="button" class="btn btn-outline-dark d-flex justify-content-between"><span class="me-2">문화시설</span><i class="fa-solid fa-gamepad"></i></button>
-        <button type="button" class="btn btn-outline-dark d-flex justify-content-between"><span class="me-2">축제</span><i class="fa-solid fa-ticket"></i></button>
-      </nav>
-    </div>
-    </div>
-    <router-view/>
->>>>>>> 0201c1ee4e7bbae994e095d78c9e101ad9a4e434
   </div>
 </template>
 
