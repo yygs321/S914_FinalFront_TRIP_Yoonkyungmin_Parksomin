da0f8be7429b9b1ce7dd48cbb86ab2d35fbfaacf
윤경민 <yoon1994kr@naver.com>
2023-05-25 15:03:07 +0900

    [feat] Add SignUp & Password Encode & id duplication check

--- src/components/user/TripSignup.vue
+++ src/components/user/TripSignup.vue
@@ -8,47 +8,123 @@
 
         <div id="board">
           
-          <div class="form-floating mx-auto p-2 mt-5 mb-2" style="width: 70%;">
-            <p style="float:left">Id</p>
-            <input type="text" class="form-control" id="id" placeholder="ssafy">
-            <!--<label for="floatingInput">Email address</label>-->
-          </div>
-  
-          <div class="form-floating mx-auto p-2  mb-2" style="width: 70%;">
-            <p style="float:left">Email</p>
-            <input type="email" class="form-control" id="email" placeholder="ssafy@ssafy.com">
-            <!--<label for="floatingInput">Email address</label>-->
-          </div>
-  
-          <div class="form-floating mx-auto p-2  mb-2" style="width: 70%;">
-            <p style="float:left">Name</p>
-            <input type="text" class="form-control" id="name" placeholder="김싸피">
+          <div class="mx-auto p-2 mt-5 mb-2" style="width: 70%;">
+            <p class="text-start">ID</p>
+            <div class="d-flex ">
+              <input type="text" class="form-control form-control-lg vertical-center-input" id="id" v-model="id">
+              <a class="ms-2 rounded-pill btn btn-dark col-2 h-100" id="dupCheck" @click="dupCheck">중복확인</a>
+            </div>
             <!--<label for="floatingInput">Email address</label>-->
           </div>
           
-          <div class="form-floating mx-auto p-2  mb-2" style="width: 70%;">
+          <div class="mx-auto p-2  mb-2" style="width: 70%;">
             <p type="password" style="float:left">Password</p>
-            <input type="password" class="form-control" id="password" placeholder="password">
+            <input type="password" class="form-control form-control-lg vertical-center-input" id="password" v-model="password">
+          </div>
+
+          <div class="mx-auto p-2  mb-2" style="width: 70%;">
+            <p style="float:left">Name</p>
+            <input type="text" class="form-control form-control-lg vertical-center-input" id="name" v-model="username">
+            <!--<label for="floatingInput">Email address</label>-->
           </div>
   
-          <div class="form-floating mx-auto p-2 mb-5" style="width: 70%;">
+          <div class="mx-auto p-2  mb-2" style="width: 70%;">
+            <p style="float:left">Email</p>
+            <input type="email" class="form-control form-control-lg vertical-center-input" id="email" v-model="email">
+            <!--<label for="floatingInput">Email address</label>-->
+          </div>
+  
+          <div class="mx-auto p-2 mb-5" style="width: 70%;">
             <p style="float:left">Address</p>
-            <input type="text" class="form-control" id="address" placeholder="서울시 강남구 역삼동">
+            <input type="text" class="form-control form-control-lg vertical-center-input" id="address" v-model="address">
             <!--<label for="floatingInput">Email address</label>-->
           </div>
 
         </div>
 
 
-        <button id="submit" class="btn btn-lg btn-primary mb-2 rounded-pill" style="width: 15%;" type="submit">Sign up</button>
+        <button class="btn btn-lg btn-primary mb-2 rounded-pill" style="width: 15%;" id="submit" @click="signUp">Sign up</button>
       </form>
     </div>
   </header>
 </template>
 
 <script>
+import http from "@/axios/axios-common.js"
 export default {
   name: 'TripSignup',
+  data(){
+    return{
+      id:"",
+      username:"",
+      password:"",
+      email:"",
+      address:"",
+      dupChk: false 
+    }
+  },
+  watch: {
+		id: {
+				immediate: true,
+				handler() {
+					this.dupChk = false
+				},
+		},
+
+	},
+  methods: {
+    signUp(){
+      if (this.id == undefined || this.id == "")
+        alert('아이디를 입력해주세요')
+      else if (this.dupCheck)
+        alert('아이디 중복체크를 해주세요')
+      else if (this.username == undefined || this.username == "")
+        alert('이름을 입력해주세요')
+      else if (this.email == undefined || this.email == "")
+        alert('이메일을 입력해주세요')
+      else if (this.address == undefined || this.address == "")
+        alert('주소를 입력해주세요')
+      else {
+        http.post("/users", {
+          id: this.id,
+          name: this.username,
+          pass: this.password,
+          email: this.email,
+          address: this.address
+        })
+        .then((response) => 
+        {
+          response;
+          this.$router.push({name:"home"})
+        })
+        .catch(function(error){
+          alert(error)
+        })        
+      }
+
+		},
+
+    dupCheck(){
+      if (this.id == undefined || this.id == "")
+        alert('아이디를 입력해주세요')
+      else{
+        http.get("/users/check/" + this.id)
+        .then((response) => 
+        {
+          if (response.data) {
+            this.dupChk = response.data
+            alert('사용할 수 있는 아이디입니다.')
+          }
+          else
+            alert('이미 존재하는 아이디입니다.')
+        })
+        .catch(function(error){
+          alert(error)
+        })
+      }
+
+		},
+  }
 }
 </script>
 
@@ -77,6 +153,11 @@ button#signin {
 
 #board{
   font-weight: bold;
+}
+
+.vertical-center-input {
+    display: flex;
+    align-items: center;
 }
 
 
